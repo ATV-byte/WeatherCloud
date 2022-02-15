14e0fc3c95933fa200142dc8ca80e1d555221470
Tudorache Vlad Adrian <57212902+ATV-byte@users.noreply.github.com>
2022-02-15 16:28:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import json
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/home', methods=['GET', 'POST'])
 def home():  # put application's code here
     useful_data = {}
     if request.method == 'POST':
